7caf3a33290045b251fa052e9660e3e880eae8e4
Javier Casas <javier.casasmarin@niko.eu>
2019-05-16 17:09:25 +0200

    Add jansson lib example

---
+++ json/jansson_lib/create_json_example.c
@@ -0,0 +1,62 @@
+/*
+https://github.com/akheron/jansson
+sudo apt-get install libjansson-dev
+gcc -o test test.c -ljansson
+
+json:
+{
+ "stream_type":"SUB_STREAM",
+ "end_time":123456789,
+ "devices":[
+	{
+	"device":"uuid_1",
+	"property":"prop1"
+	},
+	{
+	"device":"uuid_2",
+	"property":"prop2"
+	},
+	{
+	"device":"uuid_3",
+	"property":"prop3"
+	}
+ ]
+}
+*/
+#include <stdio.h>
+#include <string.h>
+#include <jansson.h>
+
+
+int main(void)
+{
+	char *s;
+	const char *devices[] = {"uuid_1", "uuid_2", "uuid_3"};
+	const char *properties[] = {"prop1", "prop2", "prop3"};
+	int i; 
+
+	json_t *root = json_object();
+	json_t *dev_prop = json_object();
+	json_t *dev_prop_pairs = json_array();
+
+	for(i=0; i < 3; i++){
+		json_t *tmp = json_object();
+		json_object_set_new(tmp, "device", json_string(devices[i]));
+		json_object_set_new(tmp, "property", json_string(properties[i]));
+		json_array_append_new(dev_prop_pairs, tmp);
+	}
+
+	json_object_set_new(root, "stream_type", json_string("SUB_STREAM"));
+	json_object_set_new(root, "end_time", json_integer(123456789));
+	json_object_set_new(root, "devices", dev_prop_pairs);
+
+
+	s = json_dumps(root, 0);
+
+	puts(s);
+
+	json_decref(root);
+	free(s);
+
+	return 0;
+}
