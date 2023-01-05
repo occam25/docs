ea6cd0355ee150c7a50379ba3dacb1d5a6c7427e
Javier Casas <javiercasas@geotab.com>
2023-01-05 18:10:18 +0100

    Update neovim config

--- neovim/.config/nvim/lua/javi/core/colorscheme.lua
+++ neovim/.config/nvim/lua/javi/core/colorscheme.lua
@@ -1,10 +1,21 @@
 -- set colorscheme to nightfly with protected call
 -- in case it isn't installed
 
-local colorscheme = "colorscheme nightfly"
---local colorscheme = "colorscheme rose-pine"
-local status, _ = pcall(vim.cmd, colorscheme)
-if not status then
-  print("Colorscheme not found!") -- print error if colorscheme not installed
-  return
+-- local colorscheme = "colorscheme nightfly"
+-- --local colorscheme = "colorscheme rose-pine"
+-- local status, _ = pcall(vim.cmd, colorscheme)
+-- if not status then
+--   print("Colorscheme not found!") -- print error if colorscheme not installed
+--   return
+-- end
+
+require("rose-pine").setup({
+	disable_background = true,
+})
+
+function ColorMyPencils(color)
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
 end
+
+ColorMyPencils()
