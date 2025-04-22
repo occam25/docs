f2198d885cc0f4d853ab3904e22a16ce0c013982
Javi <javier.casas@wistat-solutions.com>
2025-04-22 11:50:37 +0200

    update nvim setup script

--- neovim/nvim_setup.sh
+++ neovim/nvim_setup.sh
@@ -1,37 +1,27 @@
 #!/bin/bash
 
 sudo add-apt-repository ppa:neovim-ppa/stable
-sudo add-apt-repository ppa:neovim-ppa/unstable
+# add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
-sudo apt install neovim python3-dev python3-pip python3-neovim xclip
-
-if [ ! -d ~/.local/share/nvim/site/pack/packer/start/packer.nvim ]; then
-	echo "Cloning packer repo"
-	git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-fi
+sudo apt-get install neovim python3-dev python3-pip python3-neovim xclip
 
 if [ ! -f ~/.local/share/fonts/MesloLGMNerdFont-Regular.ttf ]; then
 	echo "Installing Meslo fonts"
 	wget -P /tmp https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Meslo.zip
-
 	unzip /tmp/Meslo.zip -d ~/.local/share/fonts/
 	rm *Windows* 2>/dev/null
 	fc-cache -fv
 fi
 
-apt install python3.10-venv
-
 sudo apt-get install npm ripgrep bear clangd ninja-build python3.10-venv
 
-if [ ! -d ~/docs ]; then
-	git clone https://github.com/occam25/docs.git ~/
+if [ ! -d ~/dotfiles ]; then
+	git clone https://github.com/occam25/dotfiles.git ~/
 fi
 
 if [ -d ~/.config/nvim  ]; then
 	echo "Removing old configuration"
 	rm -r ~/.config/nvim 
 fi
-cp -r ~/docs/neovim/.config/nvim ~/.config/nvim 
-rm -r ~/.config.nvim/plugin 2>/dev/null
+cp -r ~/dotfiles/.config/nvim ~/.config/nvim
 
-echo "WARNING: The script pylsp is installed in '/home/javiercasas/.local/bin' which is not on PATH."
