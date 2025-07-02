027e5c99a51d799535da1db15805259f43ff40c7
Javier Casas <occam25@yahoo.es>
2025-07-02 17:51:39 +0200

    update nvim config

---
+++ neovim/.config/nvim/lua/plugins/import/copilot.lua
@@ -0,0 +1,18 @@
+return {
+    {
+        "zbirenbaum/copilot.lua",
+        event = 'InsertEnter',
+        opts = {
+            panel = {
+                enabled = false,
+            },
+            suggestion = {
+                auto_trigger = true,
+                hide_during_completion = false,
+                keymap = {
+                    accept = '<C-y>',
+                },
+            },
+        },
+    }
+}
--- neovim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ neovim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -38,7 +38,6 @@ cmp.setup({
   }),
   -- sources for autocompletion
   sources = cmp.config.sources({
-    { name = "copilot" }, -- copilot
     { name = "nvim_lsp" }, -- lsp
     { name = "luasnip" }, -- snippets
     { name = "buffer" }, -- text within current buffer
