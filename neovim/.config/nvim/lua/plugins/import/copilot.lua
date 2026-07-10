7add795aeaec03ce815ec595eac9722de23e7417
Javier Casas <occam25@yahoo.es>
2026-07-10 12:31:25 +0200

    comment out avante nvim plugin

--- neovim/.config/nvim/lua/plugins/import/copilot.lua
+++ neovim/.config/nvim/lua/plugins/import/copilot.lua
@@ -1,18 +1,18 @@
 return {
-    {
-        "zbirenbaum/copilot.lua",
-        event = 'InsertEnter',
-        opts = {
-            panel = {
-                enabled = false,
-            },
-            suggestion = {
-                auto_trigger = true,
-                hide_during_completion = false,
-                keymap = {
-                    accept = '<C-a>',
-                },
-            },
-        },
-    }
+    -- {
+    --     "zbirenbaum/copilot.lua",
+    --     event = 'InsertEnter',
+    --     opts = {
+    --         panel = {
+    --             enabled = false,
+    --         },
+    --         suggestion = {
+    --             auto_trigger = true,
+    --             hide_during_completion = false,
+    --             keymap = {
+    --                 accept = '<C-a>',
+    --             },
+    --         },
+    --     },
+    -- }
 }
